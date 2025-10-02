d4ef4240cb05aeedae0fbfb1af0ae7d6201c101d
khandokar <khandokarsabbir@gmail.com>
2025-10-01 23:11:56 -0700

    Client Script added

---
+++ SignalRTutorial/ClientScript/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    library: {
+      name: 'MYAPP',
+      type: 'var'
+    },
+    filename: 'app-client.js',
+    path: path.resolve(__dirname, '../wwwroot/js'),
+  }
+};
