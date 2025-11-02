b38b93c8d7115fa6db10320e908147ef7bc19fae
khandokar <khandokarsabbir@gmail.com>
2025-11-01 20:20:10 -0700

    Hub added

---
+++ SignalRTutorial/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTutorial.Hubs;
+
+public class ChatHub : Hub
+{
+       public async Task NewMessage(long username, string message) =>
+        await Clients.All.SendAsync("messageReceived", username, message);
+}
